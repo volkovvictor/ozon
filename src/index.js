2b17b4a2ff7c1db1b1b13607a8d358e7254c7427
Victor Volkov <vityan2504@gmail.com>
2022-05-19 19:41:21 +0300

    написал функцию рендера и фильтрации по поиску, по категории и по цене

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 import cart from "./modules/cart";
-import second from "./modules/second";
+import load from "./modules/load";
+import search from "./modules/search";
+import catalog from "./modules/catalog";
+import rangePrice from "./modules/rangePrice";
 
 cart();
-second();
+load();
+search();
+catalog();
+rangePrice();
